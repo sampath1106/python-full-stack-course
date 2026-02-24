b9d53005dcda4caf9c8554e29d6f2ffd94393703
sampath1106 <murududdiajay@gmail.com>
2026-02-24 13:34:09 +0530

    Add files via upload

---
+++ apend.py
@@ -0,0 +1,25 @@
+'''array'''
+
+'''arr=[10,20,30,40,50]
+print("arr after append:",arr)
+arr.insert(5,25)
+print("arr after append:",arr)
+arr.remove(40)
+print("arr after delete:",arr)
+arr.sort()
+print("arr after sorting:",arr)
+arr.sort(reverse=True)
+print("enter after sorting desender:",arr)
+arr=sorted(arr)
+print("new sorted array:",arr)'''
+
+
+
+  '''arr=list(map(int,input("enter the element of the array by sepration:").split()))
+key=int(input("enter a number to be searched:"))
+for i in arr:
+    print("element found at index:",arr.index)
+    break
+else:
+    print("not found")''
+
