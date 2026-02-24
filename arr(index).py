d668d556688c462b4e050f6f70b7ce0d2fc5fd6f
sampath1106 <murududdiajay@gmail.com>
2026-02-24 13:33:27 +0530

    Add files via upload

---
+++ arr(index).py
@@ -0,0 +1,20 @@
+arr=[33,25,52,63,85,-23]
+print(arr)
+print(arr[3])
+print(arr[-1])
+
+                                                                                                                                       
+
+num=list(map(int,input("enter element with space sepration").split()))
+print(num)
+
+
+num=list(map(int,input("enter element with space sepration").split()))
+
+for i in num:
+    i+=10
+    print(i,end=" ")
+
+
+
+
