fbe1d566c1a746b7559f2b5ed050fc142bdaf296
sampath1106 <murududdiajay@gmail.com>
2026-03-06 14:30:32 +0530

    Add files via upload

---
+++ strings in python.py
@@ -0,0 +1,33 @@
+s1 = 'GfG'  # single quote
+s2 = "GfG"  # double quote
+print(s1)
+print(s2)
+
+
+s = "GeeksforGeeks"
+print(s[0])   # first character
+print(s[4])   # 5th character
+
+
+s = "GeeksforGeeks"
+print(s[-10])   # 3rd character
+print(s[-5])    # 5th character from end
+
+
+
+
+s = "GeeksforGeeks"
+print(s[1:4])    # characters from index 1 to 3
+print(s[:3])     # from start to index 2
+print(s[3:])     # from index 3 to end
+print(s[::-1])   # reverse string
+
+
+s = "Python"
+for char in s:
+    print(char)
+
+
+name = "Alice"
+age = 22
+print(f"Name: {name}, Age: {age}")    
