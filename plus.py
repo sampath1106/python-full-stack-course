8978ac2c34f7fc79ebb02f2671cd3858ccef3054
sampath1106 <murududdiajay@gmail.com>
2026-03-07 17:38:04 +0530

    Add files via upload

---
+++ plus.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    for j  in range(5):
+        if i==2  or j==2 or j==n-1:
+            
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
