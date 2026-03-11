3a0e6e863cd76580535f7a7a3e8ebdadf2d01934
sampath1106 <murududdiajay@gmail.com>
2026-03-11 17:43:33 +0530

    Add files via upload

---
+++ multi level.py
@@ -0,0 +1,13 @@
+class parent:
+    def d1(self):
+        
+        print("hii raa:")
+class child(parent):
+    def c1(self):
+        
+        print("amma nana:")
+p=parent()
+p.d1()
+c=child()
+c.c1()
+c.d1()
