a2a19a9f0b85c3ebc9464d3b25a390be495295df
sampath1106 <murududdiajay@gmail.com>
2026-03-11 16:41:40 +0530

    Add files via upload

---
+++ n capsilation.py
@@ -0,0 +1,8 @@
+class bank:
+    def __init__(self,amount):
+        self.__amount=amount
+    #def balance(self):
+        #print(self.__amount)
+b1=bank(999)
+#b1.balance()
+print(b1._bank__amount)
