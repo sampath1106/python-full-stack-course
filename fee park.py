ae1ad1fbdd653623d915f6549d5fc5faf5baf9c7
sampath1106 <murududdiajay@gmail.com>
2026-03-29 07:22:52 +0530

    Add files via upload

---
+++ fee park.py
@@ -0,0 +1,14 @@
+
+month=int(input("enter a  month:"))
+if month==1:
+    print(1000)
+elif month==3:
+    print(5000)
+elif month>=4 and month<=6:
+    print(9000)
+elif month==9:
+    print(12000)
+elif month==12:
+    print(15000)
+else:
+    print("invalid input:")
